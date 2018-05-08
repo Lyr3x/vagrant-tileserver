7ed610a7cc9a33fd6d0d7cb6ad145914d4b9a61a
Kai Bepperling <kaibepperling@MacBook-Pro-2.local>
2018-05-08 09:00:53 +0200

    added osm-carto hash to the last working state. Should be reworked

--- scripts/import.sh
+++ scripts/import.sh
@@ -11,6 +11,7 @@ eval "$SHAPE_SCRIPT"
 
 echo "-> Import OpenStreetMap files into database"
 cd /usr/share/openstreetmap-carto
+git checkout 612b2f9f8135388db55a6c31a746941afa9d9c85
 for mapfile in $BASEDIR/blob/*.osm.pbf; do
   echo "-> Import: $mapfile"
   [[ -z "${OSM2PGSQL_FLAGS}" ]] && export OSM2PGSQL_FLAGS="-c"
